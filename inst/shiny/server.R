eeeb7e590d3c14eaaf32c1952bdc008ad0444129
kdaily <kdaily@users.noreply.github.com>
2016-06-02 15:45:03 -0700

    server code for rendering the plot

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -1,1 +1,7 @@
-server <- function(input, output) { }
+server <- function(input, output) { 
+  output$plot <- renderRcytoscapejs({
+    cyNetwork <- createCytoscapeJsNetwork(nodeData, edgeData)
+    rcytoscapejs(nodeEntries=cyNetwork$nodes, edgeEntries=cyNetwork$edges)
+  })
+  
+}
