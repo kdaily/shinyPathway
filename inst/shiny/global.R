8b92aac32fdec08d272641d706a8a26907c01ef3
kdaily <kdaily@users.noreply.github.com>
2016-06-02 12:06:52 -0700

    change network name

--- inst/shiny/global.R
+++ inst/shiny/global.R
@@ -1,6 +1,6 @@
 library(shiny)
 library(paxtoolsr)
 
-sif <- toSif(system.file("extdata", 
-                         "biopax3-short-metabolic-pathway.owl",
-                         package = "paxtoolsr"))
+sifNetwork <- toSif(system.file("extdata", 
+                                "biopax3-short-metabolic-pathway.owl",
+                                package = "paxtoolsr"))
