078c1c83d41bb93ff5b982251cd1721d1ba32865
kdaily <kdaily@users.noreply.github.com>
2016-06-03 15:15:38 -0700

    use pathway commons search to get a pathway

--- inst/shiny/global.R
+++ inst/shiny/global.R
@@ -2,6 +2,22 @@ library(shiny)
 library(paxtoolsr)
 library(rcytoscapejs)
 
-sifNetwork <- toSif(system.file("extdata", 
-                                "biopax3-short-metabolic-pathway.owl",
-                                package = "paxtoolsr"))
+searchResults <- searchPc(q = "glycolysis", type = "pathway")
+
+searchResults <- xpathSApply(searchResults, 
+                             "/searchResponse/searchHit/uri", 
+                             xmlValue)
+
+foo <- getPc(uri = searchResults[67])
+
+biopaxFile <- tempfile()
+saveXML(foo, file=biopaxFile)
+
+sifNetwork <- toSif(inputFile = biopaxFile)
+
+edgeData <- sifNetwork %>% 
+  rename(source=PARTICIPANT_A, target=PARTICIPANT_B)
+
+nodes <- unique(c(edgeData$source, edgeData$target))
+
+nodeData <- data.frame(id=nodes, name=nodes, stringsAsFactors=FALSE)
